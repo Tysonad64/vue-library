97a08f4642edcd20d512915a07a46d471cf3e841
Tyson <tysonad3026@outlook.com>
2025-09-26 16:48:21 -0700

    Added a main greeting and base components
    made greeting for the app.vue added components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+const footerMessage = ref('© 2025 Dark Matter Books')
+</script>
+
+<template>
+    <footer>
+        <span>{{ footerMessage }}</span>
+    </footer>
+</template>
+
+<style scoped>
+footer span{
+    font-size: 1rem;
+    text-align: center;
+    margin: 1rem 0;
+    padding: 0.5rem;
+    display: block;
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref} from 'vue'
+
+const welcomeMessage = ref('Welcome to DMB HQ! Your portal to the ever expanding universe of books!')
+</script>
+
+<template>
+    <main>
+        <h2>{{ welcomeMessage }}</h2>
+    </main>
+</template>
+
+<style scoped>
+main h2{
+    font-size: 1.5rem;
+    text-align: center;
+    margin: 1rem 0;
+}
+</style>
